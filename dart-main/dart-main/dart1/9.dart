6148b9f61e33b9dbc7e4f6ec74b27d61fa4fecaa
Hyeonseo <90162269+Hyeonseo1021@users.noreply.github.com>
2024-10-24 10:38:49 +0900

    Update 9.dart

--- dart-main/dart-main/dart1/9.dart
+++ dart-main/dart-main/dart1/9.dart
@@ -1,17 +1,18 @@
 import 'dart:io';
+
 void main() {
-  for(int i=0; i<5; i++) {
-    for(int j=0; j<5; j++) {
-      if ((i == 1 || i == 3) && j == 2) {
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      if ((i == 1 || i == 3) && (j == 1 || j == 3)) {
         stdout.write(" ");
       } 
-      // i==2 줄에서는 j==1, j==3일 때 공백
       else if (i == 2 && (j == 1 || j == 3)) {
         stdout.write(" ");
-      }  else {
+      } 
+      else {
         stdout.write("*");
       }
     }
-    print("");
+    print(""); // 줄 바꿈
   }
-} 
+}
